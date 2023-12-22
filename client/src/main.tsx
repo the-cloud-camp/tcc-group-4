df93e306e5ce4601905fa2acb9bde30220a970d3
dude <supatatdisk@gmail.com>
2023-12-22 21:47:05 +0700

    chore: remove basename

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,21 +7,16 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import TxnDetail from './pages/TxnDetail'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Index />,
-    },
-    {
-      path: '/:txnId',
-      element: <TxnDetail />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: '/group-4/client',
+    path: '/',
+    element: <Index />,
   },
-)
+  {
+    path: '/:txnId',
+    element: <TxnDetail />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
